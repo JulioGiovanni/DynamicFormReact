78b0001b3e0a3bb764e03f22e9a8dc3eb3bf2cc0
Giovanni Flores <jg250998@gmail.com>
2022-01-12 16:23:48 -0600

    Updates

--- src/store/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore, combineReducers } from "redux";
-import { fieldReducer } from "../reducers/fieldReducer";
-
-const reducer = combineReducers({
-  fields: fieldReducer
-});
-
-export const store = createStore(reducer);
--- src/types/types.js
+++
@@ -1,6 +0,0 @@
-export const types = {
-    add: "[Fields] add Field",
-    delete: "[Fields] delete Field",
-
-  };
-  
